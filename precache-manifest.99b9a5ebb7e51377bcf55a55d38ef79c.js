78628ef500178eb907302971dc5451ed0d88984b
manny2384 <gonzalezeh2015@gmail.com>
2020-09-13 16:52:12 -0700

    Updates

---
+++ precache-manifest.99b9a5ebb7e51377bcf55a55d38ef79c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e53ab0426f4049ccac53d185bc1b8b",
+    "url": "/DataAnalysis/index.html"
+  },
+  {
+    "revision": "399e40e3090b4689abae",
+    "url": "/DataAnalysis/static/css/main.5f743be5.chunk.css"
+  },
+  {
+    "revision": "d634b92862562113eb0e",
+    "url": "/DataAnalysis/static/js/2.fe640a42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DataAnalysis/static/js/2.fe640a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399e40e3090b4689abae",
+    "url": "/DataAnalysis/static/js/main.75f1c6d9.chunk.js"
+  },
+  {
+    "revision": "7e571a1a88e81ff1a43b",
+    "url": "/DataAnalysis/static/js/runtime-main.185c744b.js"
+  }
+]);
